e9d2162f41c7a37fb1e5f90e4f0ad5b4cf06f6c5
unknown <islandhuynh@gmail.com>
2021-02-26 21:34:41 -0500

    impemented redux store to typescript react

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-class App extends React.Component {
-    render() {
-        return <div> Hi There</div>
-    }
-}
+const store = createStore(reducers, applyMiddleware(thunk)); 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.querySelector('#root')
+);
+
+// interface AppProps {
+//     color?: string;
+// }
+
+// const App = (props: AppProps): JSX.Element => {
+//     return (
+//         <div>{props.color}</div>
+//     )
+// };
+
+// class App extends React.Component<AppProps> {
+//     state = { counter: 0 };
+
+//     onIncrement = (): void => {
+//         this.setState({ counter: this.state.counter + 1})
+//     };
+
+//     onDecrement = (): void => {
+//         this.setState({ counter: this.state.counter - 1})
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.onIncrement}>Incremenet</button>
+//                 <button onClick={this.onDecrement}>Decremenet</button>
+//                 {this.state.counter}
+//             </div>
+//         )
+//     }
+// }
